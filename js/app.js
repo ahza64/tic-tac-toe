71fbdfe8cb9e1eccb1c2f4c4e2907c67a7f16726
ahza64 <ahza64@yahoo.com>
2016-03-19 13:10:28 -0700

    required code

--- js/app.js
+++ js/app.js
@@ -2,5 +2,24 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  var turn = "x";
+  $('.box').on('click',
+    function whosGonnaPlay(event){
+        for(i = 0; i < 9; i ++){
+          if(($(this).text() !== "X") && ($(this).text() !== "O")){
+            if(turn){
+              $(this).text("X");
+              turn = "";
+            }else{
+              $(this).text("O");
+              turn = "x";
+            }
+          }
+        }
+  });
 
+  $('button').on('click',
+    function LetsTryEmAgain(event){
+      $('.box').text('');
+  });
 });
