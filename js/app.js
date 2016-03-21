c1b8146500505d199664cc4a0542397e3f3d29d7
ahza64 <ahza64@yahoo.com>
2016-03-21 00:14:28 -0700

    basic with image and sound bonus

--- js/app.js
+++ js/app.js
@@ -2,24 +2,125 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-  var turn = "x";
-  $('.box').on('click',
-    function whosGonnaPlay(event){
-        for(i = 0; i < 9; i ++){
-          if(($(this).text() !== "X") && ($(this).text() !== "O")){
-            if(turn){
-              $(this).text("X");
-              turn = "";
-            }else{
-              $(this).text("O");
-              turn = "x";
-            }
+
+  $.extend({
+   playSound: function(){
+     return $(
+       '<audio autoplay="autoplay" style="display:none;">' +
+        '<source src="' + arguments[0] + '.mp3" />' +
+        '<source src="' + arguments[0] + '.ogg" />' +
+        '<embed src="' + arguments[0] + '.mp3" hidden="true" autostart="true" loop="false" class="playSound" />' +
+        '</audio>'
+     ).appendTo('body');
+   }
+ });
+
+  var turn = 0;
+
+    $('.box').on('click',
+      function whosGonnaPlay(event){
+        if(($(this).text() !== "X") && ($(this).text() !== "O")){
+          if(turn % 2 === 0){
+            $(this).text("X");
+            $(this).prepend('<img src="https://media.giphy.com/media/qSVMNkreyWbza/giphy.gif"/>');
+            $('.whosTurn').text('Go OOOO!!!');
+            turn ++;
+            $.playSound('http://www.thesoundarchive.com/starwars/swvader02');
+          }else{
+            $(this).text("O");
+            $(this).prepend('<img src="https://media.giphy.com/media/fL8x5Zjbiw2VG/giphy.gif"/>');
+            $.playSound('http://www.thesoundarchive.com/starwars/dange-disturb');
+            $('.whosTurn').text('Go X!');
+            turn ++;
           }
         }
-  });
+        isWinner();
+        if (turn === 8){
+          $('.whosTurn').text('Draw');
+        }
+    });
 
-  $('button').on('click',
+  $('#boardReset').on('click',
     function LetsTryEmAgain(event){
       $('.box').text('');
+      $('.whosTurn').text('Go X!');
+      $('.Xwins').text('');
+      $('.Owins').text('');
+      turn = 0;
   });
+
+  $('#gameReset').on('click',
+    function relaodPage(event){
+      window.location.reload();
+    });
+
+  var ticTacToeBoard = $('.box');
+  function isWinner() {
+    if($(ticTacToeBoard).eq(0).text() === 'X' &&
+       $(ticTacToeBoard).eq(1).text() === 'X' &&
+       $(ticTacToeBoard).eq(2).text() === 'X'){
+        return $('.Xwins').text("X wins");
+    }else if($(ticTacToeBoard).eq(3).text() === 'X' &&
+             $(ticTacToeBoard).eq(4).text() === 'X' &&
+             $(ticTacToeBoard).eq(5).text() === 'X'){
+        return $('.Xwins').text("X wins");
+    }else if($(ticTacToeBoard).eq(6).text() === 'X' &&
+             $(ticTacToeBoard).eq(7).text() === 'X' &&
+             $(ticTacToeBoard).eq(8).text() === 'X'){
+        return $('.Xwins').text("X wins");
+    }else if($(ticTacToeBoard).eq(0).text() === 'X' &&
+             $(ticTacToeBoard).eq(3).text() === 'X' &&
+             $(ticTacToeBoard).eq(6).text() === 'X'){
+        return $('.Xwins').text("X wins");
+    }else if($(ticTacToeBoard).eq(1).text() === 'X' &&
+             $(ticTacToeBoard).eq(4).text() === 'X' &&
+             $(ticTacToeBoard).eq(7).text() === 'X'){
+        return $('.Xwins').text("X wins");
+    }else if($(ticTacToeBoard).eq(2).text() === 'X' &&
+             $(ticTacToeBoard).eq(5).text() === 'X' &&
+             $(ticTacToeBoard).eq(8).text() === 'X'){
+        return $('.Xwins').text("X wins");
+    }else if($(ticTacToeBoard).eq(0).text() === 'X' &&
+             $(ticTacToeBoard).eq(4).text() === 'X' &&
+             $(ticTacToeBoard).eq(8).text() === 'X'){
+        return $('.Xwins').text("X wins");
+    }else if($(ticTacToeBoard).eq(2).text() === 'X' &&
+             $(ticTacToeBoard).eq(4).text() === 'X' &&
+             $(ticTacToeBoard).eq(6).text() === 'X'){
+        return $('.Xwins').text("X wins");
+    }else if($(ticTacToeBoard).eq(0).text() === 'O' &&
+             $(ticTacToeBoard).eq(1).text() === 'O' &&
+             $(ticTacToeBoard).eq(2).text() === 'O'){
+        return $('.Owins').text("O wins");
+    }else if($(ticTacToeBoard).eq(3).text() === 'O' &&
+             $(ticTacToeBoard).eq(4).text() === 'O' &&
+             $(ticTacToeBoard).eq(5).text() === 'O'){
+        return $('.Owins').text("O wins");
+    }else if($(ticTacToeBoard).eq(6).text() === 'O' &&
+             $(ticTacToeBoard).eq(7).text() === 'O' &&
+             $(ticTacToeBoard).eq(8).text() === 'O'){
+        return $('.Owins').text("O wins");
+    }else if($(ticTacToeBoard).eq(0).text() === 'O' &&
+             $(ticTacToeBoard).eq(3).text() === 'O' &&
+             $(ticTacToeBoard).eq(6).text() === 'O'){
+        return $('.Owins').text("O wins");
+    }else if($(ticTacToeBoard).eq(1).text() === 'O' &&
+             $(ticTacToeBoard).eq(4).text() === 'O' &&
+             $(ticTacToeBoard).eq(7).text() === 'O'){
+        return $('.Owins').text("O wins");
+    }else if($(ticTacToeBoard).eq(2).text() === 'O' &&
+             $(ticTacToeBoard).eq(5).text() === 'O' &&
+             $(ticTacToeBoard).eq(8).text() === 'O'){
+        return $('.Owins').text("O wins");
+    }else if($(ticTacToeBoard).eq(0).text() === 'O' &&
+             $(ticTacToeBoard).eq(4).text() === 'O' &&
+             $(ticTacToeBoard).eq(8).text() === 'O'){
+        return $('.Owins').text("O wins");
+    }else if($(ticTacToeBoard).eq(2).text() === 'O' &&
+             $(ticTacToeBoard).eq(4).text() === 'O' &&
+             $(ticTacToeBoard).eq(6).text() === 'O'){
+        return $('.Owins').text("O wins");
+    }
+  }
+
 });
